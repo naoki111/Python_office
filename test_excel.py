ac6f794ddf8a645039f9ff05a1430aa887d2ce19
naoki <2004240028yn@edu.oca.ac.jp>
2022-05-21 16:30:39 +0900

    commit

--- test_excel.py
+++ test_excel.py
@@ -4,7 +4,24 @@ wb = Workbook()
 # grab the active worksheet
 ws = wb.active
 
+ws1 = wb.create_sheet("Mysheet") # insert at the end (default)
+
+ws1 = wb['Mysheet']
 # Data can be assigned directly to cells
+
+cells = ws1['A1':'J10']
+
+# 変数iの初期値を0にします。
+i = 1
+
+for row in cells:
+    # for文を利用して、変数rowがある限り、変数cellに1個づつ代入します。
+    for cell in row:
+        # 変数cellの値に、変数iを代入します。
+        cell.value = i
+        # 変数cellの値に、変数iを代入します。
+        i += 1
+
 ws['A1'] = 42
 
 # Rows can also be appended
