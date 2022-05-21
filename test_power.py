4ba2c0d51dc97992f5ffdded99f68588720f51f2
naoki <2004240028yn@edu.oca.ac.jp>
2022-05-21 17:45:12 +0900

    commit

--- test_power.py
+++ test_power.py
@@ -9,4 +9,15 @@ subtitle = slide.placeholders[1]
 title.text = "Hello, World!"
 subtitle.text = "python-pptx was here!"
 
+count = 0
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                count += len(run.text)
+
+print(f'パワーポイント内の文字数は{count}個です。')
+
 prs.save('test.pptx')
--- test_word.py
+++ test_word.py
@@ -5,4 +5,11 @@ document = Document()
 document.add_heading('簡単なWordドキュメントのタイトル', 0)
 document.add_paragraph('簡単なWordドキュメントのテキスト')
 
+document.add_picture('png.png')
+
+count = 0
+for data in document.paragraphs:
+    count += len(data.text)
+print(f'sample.docxの中の文字数は{count}個です。')
+
 document.save('sample.docx')
